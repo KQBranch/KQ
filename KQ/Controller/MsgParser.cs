a0782ceebf62658fa3c2c2c4714f33b9e130a973
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-01-27 09:08:22 +0000

    msg parser: add

---
+++ KQ/Controller/MsgParser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Mirai_CSharp.Models;
+
+namespace KQ.Controller
+{
+    public class MsgParser
+    {
+        public static string GetMsgString(IMessageBase[] imsg)
+        {
+            // Index 0 is mirai info
+            return string.Join(null, (IEnumerable<IMessageBase>) imsg[1..]);
+        }
+    }
+}
